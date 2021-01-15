c0f8d5c722c8ed97963ec26db6d3d886da111cb4
eqbrownISF <eqbrown@isf.edu.hk>
2021-01-15 16:25:32 +0800

    vanilla test w/print tweet

--- send_tweet.py
+++ send_tweet.py
@@ -37,11 +37,11 @@ if os.path.isfile('logs/.log_encoded.bin'):
     if passed_tests["elapsed_time"].count() == len(basic_tests_list):
         test_time_sum = passed_tests.sum()["elapsed_time"]
 
-        # tweet it
+        tweet it
         tweet = f'🏁 #queuerace update 🏁\n\n{os.environ["USERNAME"]} just pushed a queue that runs all the speed tests in {test_time_sum} seconds!\n\nCan you beat that? 🏎🏎🏎'
         auth = tweepy.OAuthHandler(os.environ["API_KEY"], os.environ["API_SECRET"])
         auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"])
         api = tweepy.API(auth)
         api.update_status(tweet)
-
-        # print(tweet)
+        tweet = test_time_sum
+        print(tweet)
