60f5a2ecb86a0e2908662d01027875ef3c72e487
eqbrownISF <eqbrown@isf.edu.hk>
2021-01-18 09:04:44 +0800

    len_queue test 1

--- test_queue.py
+++ test_queue.py
@@ -20,7 +20,7 @@
 # =============================================================================
 
 import unittest
-from student_queue import Queue
+from len_queue import Queue
 
 
 class TestMin(unittest.TestCase):
